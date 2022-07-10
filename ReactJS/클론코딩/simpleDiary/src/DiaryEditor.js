cc22539896d8b965ccad47f12b16777ef52d3f88
성호  권 <maystar0503@naver.com>
2022-07-10 23:44:50 +0900

    Learn reactDiary(2)

--- ReactJS/클론코딩/simpleDiary/src/DiaryEditor.js
+++ ReactJS/클론코딩/simpleDiary/src/DiaryEditor.js
@@ -1,35 +1,78 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  // useRef는 React.MutableRefObject를 반환한다. React.MutableRefObject는 HTML DOM요소를 접근할 수 있는 기능을 한다.
+  // useRef를 통해 레퍼런스 객체를 생성 함
+  const contentInput = useRef();
   const [state, setState] = useState({
     author: "",
     content: "",
+    emotion: 1,
   });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    //console.log(state);
+    if (state.author.length < 1) {
+      //alert("작성자는 최소 1글자 이상 입력해주세요");
+      // focus
+      authorInput.current.focus();
+      // 현재 가르키는 current 프로퍼티로 불러와서 사용가능
+      // authorInput.current 는 author의 input 태그를 의미함
+      // focus: 거기로 이동하겠다.
+      return;
+    }
+    if (state.content.length < 5) {
+      //alert("일기 본문은 최소 5글자 이상 입력해주세요");
+      // focus
+      contentInput.current.focus();
+      return;
+    }
+    alert("저장 성공");
+  };
 
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput} // 레퍼런스는 authorInput를 통해 이젠 input 태그에 접근 가능하디 (매핑)
+          name="author"
           value={state.author}
-          onChange={(e) => {
-            setState({
-              ...state,
-              author: e.target.value,
-            });
-          }}
+          onChange={handleChangeState}
         />
       </div>
       <div>
         <textarea
+          ref={contentInput}
+          name="content"
           value={state.content}
-          onChange={(e) => {
-            setState({
-              ...state,
-              content: e.target.value,
-            });
-          }}
+          onChange={handleChangeState}
         />
+      </div>
+      <div>
+        오늘의 감정점수
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
