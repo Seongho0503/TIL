7e77501d0b008dc7b39a59d9cc5021554026aad2
성호  권 <maystar0503@naver.com>
2022-07-14 23:42:13 +0900

    make ReactDiary
    add dummylist

--- ReactJS/클론코딩/simpleDiary/src/App.js
+++ ReactJS/클론코딩/simpleDiary/src/App.js
@@ -1,12 +1,47 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이정환",
+    content: "하이 1",
+    emotion: 5,
+    created_date: new Date(),
+    // new Date() : 현재시간 / new Date().getTime() [millisecond로 변환]
+  },
+  {
+    id: 2,
+    author: "권성호",
+    content: "하이 2",
+    emotion: 4,
+    created_date: new Date(),
+    // new Date() : 현재시간 / new Date().getTime() [millisecond로 변환]
+  },
+  {
+    id: 3,
+    author: "이재환",
+    content: "하이 3",
+    emotion: 3,
+    created_date: new Date(),
+    // new Date() : 현재시간 / new Date().getTime() [millisecond로 변환]
+  },
+  {
+    id: 4,
+    author: "김필재",
+    content: "하이 4",
+    emotion: 4,
+    created_date: new Date(),
+    // new Date() : 현재시간 / new Date().getTime() [millisecond로 변환]
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
-      <DiaryList />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
