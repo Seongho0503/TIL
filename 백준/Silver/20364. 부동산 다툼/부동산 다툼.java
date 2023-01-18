c812255a1ee0980505128b2f8f3b1304343133f6
성호  권 <maystar0503@naver.com>
2023-01-18 09:59:20 +0900

    [Silver I] Title: 부동산 다툼, Time: 3992 ms, Memory: 103400 KB -BaekjoonHub

--- 백준/Silver/20364. 부동산 다툼/부동산 다툼.java
+++ 백준/Silver/20364. 부동산 다툼/부동산 다툼.java
@@ -8,24 +8,24 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int Q = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken()); // 땅 개수
+		int Q = Integer.parseInt(st.nextToken()); // 오리 수
 		
+		// 오리가 땅 점유하고 있는지 여부 
 		boolean[] used = new boolean[N+1];
 		for(int i = 0; i < Q; i++) {
-			int duck = Integer.parseInt(br.readLine().trim());
-			int first = 0;
-			int node = duck;
+			int duck = Integer.parseInt(br.readLine().trim()); // 오리가 원하는 땅
+			int first = 0; // 가질 수 없다면 처음 마주치는 점유된 땅의 번호
+			int node = duck; // 현재 위치의 노드 번호
 			while(node > 0) {
-				if(used[node])	
-					first = node;
-				node /= 2;
+				if(used[node]) first = node; // 갈 수 없는 땅 갱신
+				node /= 2; // 부모 노드 
 			}
 			if(first != 0)
 				System.out.println(first);
 			else {
-				System.out.println(0);
-				used[duck] = true;
+				System.out.println(0); 
+				used[duck] = true;   // 원하는 땅에 갈 수 있으면 방문처리 
 			}
 				
 		}
