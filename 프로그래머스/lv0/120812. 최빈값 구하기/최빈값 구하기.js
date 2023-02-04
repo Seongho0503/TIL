7f32e88d9eb212a37ea24df9146d38cd809da347
성호  권 <maystar0503@naver.com>
2023-02-04 20:46:21 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.37 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,10 +1,20 @@
 function solution(array) {
-    var answer = [];
+    var answer = new Array(1000).fill(null);
+    var idx = -1;
+    // var answer = new Array();
     var num = -1;
-    array.map((i) => answer[i-1]+=1)
-    array.map((j) =>{
-        if(num === array[j]) return;
-        num = Math.max(num, array[j]);
+    // 누적 
+    array.map((i) => answer[i]+=1);
+    // max 값 구하기
+    answer.map((j, id) =>{
+        if(num < j) {
+            num = j;
+            idx = id;
+        //num = Math.max(num, j);
+    }})
+    // max 중복 확인
+    answer.map((k,id)=>{
+        if(k===num && id!=idx) idx=-1;
     })
-    return num;
+    return idx;
 }
