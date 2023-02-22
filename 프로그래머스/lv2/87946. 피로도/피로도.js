ef94223bb48065387033a8fa70a5031adc0bd198
성호  권 <maystar0503@naver.com>
2023-02-22 19:12:20 +0900

    [level 2] Title: 피로도, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,18 +1,21 @@
 function solution(k, dungeons) {
-    var visited = [];
+    let visited = []; 
+    // hp = 현재 피로도 
     const travel = (hp, count) => {
-          for(var i=0; i<dungeons.length; i++){
+     for(let i=0; i < dungeons.length; i++){
+    // i번째 던전 방문 && 현재 피로도와 던전 진입 피로도 확인
         if(!visited[i] && dungeons[i][0] <= hp){
             visited[i] = true;
             cur_hp = hp - dungeons[i][1];
             travel(cur_hp, count+1);
             visited[i] = false;
         }    
-     }
+     } //for문 종료
         answer = Math.max(answer, count);
     } // travel 종료
   
-    var answer = -1;
+    let answer = -1;
+    // dfs 
     travel(k, 0);
     return answer;
 }
