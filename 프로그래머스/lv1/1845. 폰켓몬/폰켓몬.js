54fbb269b3d3ab5047858c9ce81163af29a42be7
성호  권 <maystar0503@naver.com>
2023-03-04 14:10:32 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,12 +1,12 @@
 function solution(nums) {
     // 뽑을 수 있는 포켓몬 : 길이/2
-    let len = nums.length/2;
+    let max = nums.length/2;
     // 폰켓몬 종류 : 중복 숫자 제외
-    const hash = new Set(nums)
+    const hash = new Set(nums) // 배열을 set 변환
     //console.log(hash) 〉	Set(3) { 3, 1, 2 }
     //console.log(hash.size) // hash.size : 3   
     
-    let answer = len > hash.size ? hash.size : len
+    let answer = max > hash.size ? hash.size : max
     
     return answer;
 }
