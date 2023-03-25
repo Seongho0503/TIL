e32fdd8b6558a0205d1597ebc6d08e6b7a7c84eb
성호  권 <maystar0503@naver.com>
2023-03-25 23:13:15 +0900

    [level 2] Title: 타겟 넘버, Time: 0.98 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,22 +1,51 @@
+function solution(numbers, target) {
+    let count = 0;
+    function dfs(sum, index){
+        if(index === numbers.length ){
+            if(sum === target){
+                 count++;
+            }
+            return;   
+        }
+        let cur = numbers[index];
+        
+        dfs(sum + cur, index+1);
+        dfs(sum - cur, index+1);     
+    }
+    dfs(0,0)
+    return count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // +, - 선택 dfs
 
 
-function solution(numbers, target) {
-   function dfs(count, sum) {
-      // 피연산자를 다 사용했으면
-     if(count === numbers.length){
-      // 총합이 targer 하고 같은지 확인
-       if(sum === target) answer++;
-        return;
-     }  
-       let num = numbers[count]
-       dfs(count+1,sum+num)
-       dfs(count+1,sum-num)
-   }
+// function solution(numbers, target) {
+//    function dfs(count, sum) {
+//       // 피연산자를 다 사용했으면
+//      if(count === numbers.length){
+//       // 총합이 targer 하고 같은지 확인
+//        if(sum === target) answer++;
+//         return;
+//      }  
+//        let num = numbers[count]
+//        dfs(count+1,sum+num)
+//        dfs(count+1,sum-num)
+//    }
     
-    let answer = 0;  // function 안에 function 선언하면 전역변수로 사용가능  
-    dfs(0,0)
+//     let answer = 0;  // function 안에 function 선언하면 전역변수로 사용가능  
+//     dfs(0,0)
 
-    return answer;
+//     return answer;
     
-}
+// }
