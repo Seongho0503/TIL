ca28fc8d598c7f428ee8e5f06377df538db06e90
성호  권 <maystar0503@naver.com>
2023-04-15 00:37:29 +0900

    [level 2] Title: 스킬트리, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
@@ -1,24 +1,21 @@
 function solution(skill, skill_trees) {
-    // 문자열을 배열로
-    let newArr = skill.split("");
-    //console.log(newArr); 	[ 'C', 'B', 'D' ]
-    let str = 0;
     let count = 0;
+    let str = skill.split("");
     for(let i = 0; i < skill_trees.length; i++){
-        //newArr이 포함하는 문자인 것들만(true) 반환 => join("")을 이것을 다시 문자열로 반환
-        str = skill_trees[i].split("").filter(e => newArr.includes(e)).join("");
-        //console.log(str) 아래 주석 참고
-        // 정답인 skill 만큼 길이를 자르고 비교 => 같으면 개수 증가
-        if(str === skill.substring(0, str.length)){
-            count++;
-        }
+     let newArr = skill_trees[i].split("").filter(e => str.includes(e)).join("");
+        if(newArr === skill.substring(0, newArr.length)) count++;
     }
     return count; 
 }
 
-// newArr이 포함하는 문자인 것들만(true) 반환 => join("")을 이것을 다시 문자열로 반환
-// str = skill_trees[i].split("").filter(e => newArr.includes(e))
-// [ 'B', 'C', 'D' ]
-// [ 'C', 'B', 'D' ]
-// [ 'C', 'B' ]
-// [ 'B', 'D' ]
+// console.log(skill_trees[i].split(" "))
+// [ 'BACDE' ]
+// [ 'CBADF' ]
+// [ 'AECB' ]
+// [ 'BDA' ]
+
+// console.log(skill_trees[i].split(""))
+// [ 'B', 'A', 'C', 'D', 'E' ]
+// [ 'C', 'B', 'A', 'D', 'F' ]
+// [ 'A', 'E', 'C', 'B' ]
+// [ 'B', 'D', 'A' ]
