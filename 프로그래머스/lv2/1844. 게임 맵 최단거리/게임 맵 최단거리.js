98aee312314e1b8c5302785af5ad9c6e5a3963e3
성호  권 <maystar0503@naver.com>
2023-04-15 21:00:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 33.47 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,32 +1,28 @@
-// 최단거리 bfs
-// visited 방문 배열 (한 번 방문 한 곳 다시 방문 X) 
-function solution(maps) {
+function solution (maps) {
     let count = 0;
-    const visited = Array(maps.length).fill(0).map(()=> Array(maps[0].length).fill(0))
-    // 오, 왼, 위, 아래 
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
-    // 가로 , 세로
-    let n = maps[0].length;
-    let m = maps.length;
-    let queue = [];
-    queue.push([0,0]);
+     const visited = Array(maps.length).fill().map(()=> Array(maps[0].length).fill(0))
+
+    let dx = [1,-1,0,0]
+    let dy = [0,0,1,-1]
+    let que = []
+    que.push([0,0]);
     visited[0][0] = 1;
-    while(queue.length > 0){
-        let [x, y] = queue.shift();
-        for(let i = 0; i < dx.length; i++){
-          let nx = x + dx[i];
-          let ny = y + dy[i];
-            // (등호주의) 등호를 안 붙여서 방향 이동이 안 되었다.
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m || maps[ny][nx] === 0) continue;
-            // if(visited[ny][nx] !== 0) 와 if(visited[ny][nx]) 동일
-            if(visited[ny][nx]) continue;
-                queue.push([nx, ny]);
-                visited[ny][nx] = visited[y][x] + 1;
+    
+    while(que.length > 0){
+        let [x,y] = que.shift();
+        for(let i =0; i < 4; i++){
+            let nx = x + dx[i];
+            let ny = y + dy[i];
+    // visited[ny][nx] === 0 실수로 maps[ny][nx] === 0 이라 써서 오류 생겼음.
+    if(nx < 0 || ny < 0 || nx >= maps[0].length || ny >= maps.length || maps[ny][nx] === 0) continue
+    if(visited[ny][nx]) continue;       
+        que.push([nx,ny])
+        visited[ny][nx] = visited[y][x] + 1;
         }
     }
-    count = visited[maps.length - 1][maps[0].length - 1];
-    // count=0 할당 후 !count 와 count === 0 동일
+    
+    count = visited[maps.length-1][maps[0].length-1];
     if(!count) return -1;
-    return count;
+
+    return count
 }
