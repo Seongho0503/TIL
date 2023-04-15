cca904423c394e30a35f100502457ce1772d1c35
성호  권 <maystar0503@naver.com>
2023-04-16 00:00:56 +0900

    [level 2] Title: 스킬트리, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
@@ -1,7 +1,6 @@
 function solution(skill, skill_trees) {
     var answer = 0;
     let sk = skill.split("");
-    
     for(let i = 0; i < skill_trees.length; i++){
         let magic = skill_trees[i].split("").filter(e => sk.includes(e)).join("");
         if(magic === skill.substring(0, magic.length)){
