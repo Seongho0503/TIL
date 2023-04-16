ecb70b123c4e5255dd5ee56203ad5048f3ff3a04
성호  권 <maystar0503@naver.com>
2023-04-17 04:01:29 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,10 +1,6 @@
 function solution(numbers) {
     var answer = '';
-    // sort((a,b) => (b+a)-(a+b)) = 문자로 변환된 숫자를 연결하여 비교정렬 ( '3', '30' => ('303')-('330'))
-    const highNum = numbers.map(c => c + "").sort((a,b) => (b+a) - (a+b)).join("");
-    //console.log(highNum)
-    // numbers.toString(); 	[6, 10, 2] => 	"6210"
-    //answer = numbers[0].toString().split("")
-    
-    return highNum[0] === '0' ? '0' : highNum;
+    const newArr = numbers.map(e => e + "").sort((a,b) => (b+a) - (a+b)).join("");
+    //console.log(newArr);
+    return Math.max(...numbers) === 0 ? "0" : newArr;
 }
