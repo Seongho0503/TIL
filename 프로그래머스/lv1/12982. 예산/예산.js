7aa07817610c3bef70153d235e46f3eb0a753baa
성호  권 <maystar0503@naver.com>
2023-04-21 10:43:18 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,33 +1,34 @@
+
 // function solution(d, budget) {
-//     var answer = 0;
+//     let result = 0
+//     let i = 0;
+//     // 오름차순 정렬 : 작은 예산 부터 시작하면 최대 부서에게 지급 가능
 //     d.sort((a,b) => a-b);
-//     // 투 포인터 : p q
-//     let p = 0;
-//     let q = 1;
-//     let sum = d[0];
-//     while(p <= q){
-//         if(sum > budget){
-//            sum -= d[p];
-//            p--;
-//         }else if(sum < budget){
-//             q++;
-//             sum += d[q];    
-//         }
-//         console.log("p" + p)
-//         answer++;    
+
+//     while(result <= budget) { // 이전 값 기준
+//         result += d[i];
+//         i++;
 //     }
-//     return answer;
+
+//     return i-1;
 // }
 
+
+
 function solution(d, budget) {
-    let result = 0
-    let i = 0;
+
+    // 오름차순 정렬 : 작은 예산 부터 시작하면 최대 부서에게 지급 가능
     d.sort((a,b) => a-b);
-
-    while(result<=budget) {
-        result += d[i];
-        i++;
+    let num = 0;
+    let sum = 0;
+    
+    for(let i = 0; i <d.length; i++){
+        if(sum + d[i] <= budget){ // 현재 값까지 포함
+            sum += d[i];
+            num++;
+        }else{
+            break
+        }
     }
-
-    return i-1;
+    return num;
 }
