261f0e1079fb038ceef18a06fd7a4f7df8e9d3cd
성호  권 <maystar0503@naver.com>
2023-04-27 10:24:00 +0900

    [level 3] Title: 기지국 설치, Time: 2.27 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.js
@@ -1,8 +1,24 @@
 function solution(n, stations, w) {
     var answer = 0;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-
+    let range = (w * 2) + 1; 
+    
+    // 왼쪽 사이드 : 시작 기지국 - 범위(반) - 시작점
+    let left = (stations[0] - w) - 1;
+    answer +=  Math.ceil(left / range)
+    
+    
+    // 오른쪽 사이드 : 마지막 기지국 + 범위(반)
+    let right = n - (stations[stations.length-1] + w )
+    answer +=  Math.ceil(right / range)
+    
+    // 기지국 사이 : 투 포인터로 앞 뒤의 두 기지국만 비교한다
+    for(let i = 0; i < stations.length -1; i++){
+       //for(let j = i+1; j < i+2; j++){
+           // 앞 기지국 - 뒤 기지국 -1 - 전파 범위(양방향)
+            let len = stations[i+1] - stations[i] -1 -2*w;
+            answer += Math.ceil(len / range);
+       //}
+    }
+    
     return answer;
 }
