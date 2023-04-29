c767ccc82c2141bb111e37d0b48370979bf1cda6
성호  권 <maystar0503@naver.com>
2023-04-29 22:32:12 +0900

    [unrated] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/unrated/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,31 +1,10 @@
-# SELECT
-#     CAR_ID,
-#     IF(SUM(START_DATE <= "2022-10-16" AND END_DATE >= "2022-10-16") = 0 , "대여 가능", "대여중") AVAILABILITY
-# FROM
-#     CAR_RENTAL_COMPANY_RENTAL_HISTORY
-# GROUP BY
-#     CAR_ID
-# ORDER BY
-#     CAR_ID DESC
-# ;
-
-with table1 as (
-    SELECT 
-        car_id,
-        '대여중' as 'AVAILABILITY' 
-    from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-    where 
-        start_date <= '2022-10-16' and '2022-10-16' <= end_date
-)
-
-SELECT 
-    CRCRH.car_id, 
-    IFNULL(AVAILABILITY, "대여 가능") as 'AVAILABILITY'
-FROM 
-    CAR_RENTAL_COMPANY_RENTAL_HISTORY AS CRCRH
-LEFT OUTER JOIN table1 AS t1
-ON 
-    CRCRH.car_id = t1.car_id 
-group by 
-    CRCRH.car_id 
-order by CRCRH.car_id desc;
+SELECT
+    CAR_ID,
+    IF(SUM(START_DATE <= "2022-10-16%" AND END_DATE >= "2022-10-16%") > 0 , "대여중", "대여 가능") AVAILABILITY
+FROM
+    CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY
+    CAR_ID
+ORDER BY
+    CAR_ID DESC
+;
