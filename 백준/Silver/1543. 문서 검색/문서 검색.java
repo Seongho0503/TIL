5d2a7788fe7c14eeb21e0299041cfc496fdf3b37
성호  권 <maystar0503@naver.com>
2023-05-06 18:09:44 +0900

    [Silver IV] Title: 문서 검색, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -8,26 +8,22 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String str = br.readLine();
-		String ans = br.readLine();
+		String word = br.readLine();
 		
-		int len = ans.length();
-		String check = "";
 		int count = 0;
+		int startIndex = 0;
 		
-		//System.out.println(str.length()-len+1);
-		for(int i = 0; i < str.length()-len+1; i++) {
-			check = "";
- 			for(int j = i; j < i + len; j++) {
- 				if(j < str.length()) {
- 					check += str.charAt(j);
- 				}else break;
-				
-			}
- 			//System.out.println(i + " " +check);
-			if(check.equals(ans)) {
-				count++;
-				i = i+len-1; // 조건식에서 증감된다.
-			}
+		while(true) {
+			// 해당 문자열에서 word라는 문자열을 찾고 싶고, 처음부터가 아닌 startIndex부터 찾고 싶어
+			int findIndex = str.indexOf(word, startIndex);
+			
+			// findIndex가 음수라면 찾지 못한거임
+			if(findIndex < 0) break;
+			// 찾으면 계속 진행
+			count++;
+			//start Index 갱신 (내가 찾은 첫 인덱스 + 길이만큼 뒤로 이동)
+			startIndex = findIndex + word.length();
+			
 		}
 		
 		System.out.print(count);
