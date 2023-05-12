3046a05cc8fbb0e600785db768bbe610fe15dd45
성호  권 <maystar0503@naver.com>
2023-05-12 22:37:50 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.19 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.51 ms
+메모리: 76.4 MB, 시간: 1.19 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] numbers) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        for(int i = 0; i < numbers.length-1; i++){
+            for(int j = i+1; j < numbers.length; j++){
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        
+        return list;
+    }
+}
