550e32d783c89afd5a672df7ce527496bff1f8a6
성호  권 <maystar0503@naver.com>
2023-05-13 21:25:34 +0900

    [Silver IV] Title: 주몽, Time: 204 ms, Memory: 16440 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -15,17 +15,28 @@ public class Main {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		//// 순차 정렬 할 때 0인덱스 확인 잘하기 (배열크기랑), 배열크기 1크게 잡으면 인덱스 변화시켜야함
 		Arrays.sort(arr);
 		
+		int start = 0;
+		int end = N-1;
 		int cnt = 0;
-		for(int i = 0; i < N-1; i++) {
-			for(int j =i+1; j < N; j++) {
-				int sum = arr[i] + arr[j];
-				//System.out.println(arr[i] + " " + arr[j] + "  " + sum);
-				if(sum == M) cnt++;
+		
+		// start가 end랑 같아도 안 됨 (재료는 고유 번호 = 서로 다른 번호) 
+		while(start < end) {
+			int sum = arr[start] + arr[end];
+			if(sum == M) { // start , end 둘 다 변화
+				start++;
+				end--;
+				cnt++;
+			// 값이 더 커져야 되는 상황 -> start 인덱스 증가
+			}else if(sum < M) {
+				start++;
+			// sum > num 값 더 줄여야 되는 상황 -> end 인덱스 감소
+			}else {
+				end--;
 			}
 		}
-		
 		System.out.println(cnt);
 	}
 
