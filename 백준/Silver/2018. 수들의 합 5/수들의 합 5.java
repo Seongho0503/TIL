9a7bae06a223d499c7f8f850472ddbe05a36bd0f
성호  권 <maystar0503@naver.com>
2023-05-13 18:48:32 +0900

    [Silver V] Title: 수들의 합 5, Time: 164 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,38 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	  
+	  int N = Integer.parseInt(br.readLine());
+	  
+	  int start = 1;
+	  int end = 1;
+	  int sum = start;
+	  int cnt = 0;
+	  while(start <= N) {
+		  //System.out.println(sum);
+		  if(sum == N) cnt++;
+		  if(sum < N) {
+			  end++;
+			  sum += end;
+		  }else {
+			  sum -= start;
+			  start++;
+		  }  
+	  }
+	  
+	  System.out.println(cnt);
 		
-		int[] arr = new int[N+1];
-		// 배열 요소 1부터 초기화
-		for (int i = 1; i <= N; i++) {
-			arr[i] = i;
-		}
-		// 자기 자신
-		int count = 1;
-		int end = 1;
-		int start = 1;
-		//1부터 시작 (1인덱스 값이 1)
-		int sum =1;
-		// 뒤의 인덱스가 N이랑 같아진다면 끝까지 탐색한 것
-		while(end != N) {
-			// 현재 연속 합이 N과 같은 경우
-			if(sum == N) {
-				count++;
-				end++;
-				sum += arr[end];
-			}else if(sum < N) { // 현재 연속 합이 N보다 작은 경우
-				end++;
-				sum += arr[end];
-			}else {//sum > N // 현재 연속 합이 N보다 큰 경우
-				sum -= arr[start];
-				start++;
-			}		
-		}
-		System.out.println(count);
 	}
 }
