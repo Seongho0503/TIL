62ca1b9e48016916c28d4f4ec3665d7ddec284cf
성호  권 <maystar0503@naver.com>
2023-06-13 11:51:12 +0900

    [unrated] Title: 디펜스 게임, Time: 0.76 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -4,35 +4,28 @@ class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = enemy.length;
         Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
+        
         int my = n;
         int card = k;
-        for (int i = 0; i < enemy.length; i++) {
-            my -= enemy[i];
+        
+        for(int i = 0; i < enemy.length; i++){
+            
+            n -= enemy[i];
             pq.add(enemy[i]);
-
-            if (my < 0) {
-                if (card > 0 && !pq.isEmpty()) {
-                    my += pq.poll();
+            
+            if(n < 0){
+                if(card > 0){
+                    n += pq.poll();
                     card--;
-                } else {
+                }else {
                     answer = i;
                     break;
                 }
             }
+            
         }
-
+        
+        
         return answer;
     }
 }
-
-
-/*
-
-<패스 카드 사용>
-1. 가자 큰 wave 거나
-2. 현재 죽을 때
-
-
-
-*/
