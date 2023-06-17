cf5bbf579e07565ee98687cc57cf2322e5ade047
성호  권 <maystar0503@naver.com>
2023-06-18 08:47:01 +0900

    [level 2] Title: 모음 사전, Time: 1.11 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/README.md
+++ 프로그래머스/lv2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 75.5 MB, 시간: 0.74 ms
+메모리: 75.2 MB, 시간: 1.11 ms
 
 ### 구분
 
--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,36 +1,31 @@
+import java.util.*;
+
 class Solution {
-    int result = 0;
-    boolean isEqual = false;
-
+    static String[] alp = {"A", "E", "I", "O", "U"};
+    static String target;
+    static int answer = 0;
+    static boolean isEqual = false;
     public int solution(String word) {
-        int answer = 0;
-
-        recursive(word, new StringBuilder(), new String[]{"A", "E", "I", "O", "U"});
-        answer = result;
+        target = word;
+        dfs(new StringBuilder());
         return answer;
     }
-
-    // String은 메모리 낭비 되기 때문에 StringBuilder 사용
-    public void recursive(String word, StringBuilder sb, String[] sArray){
+    
+    public void dfs(StringBuilder sb){
         
-        //목표 target 하고 동일하면 종료 equals 써야 함 
-        if(word.equals(sb.toString())){
+        if(target.equals(sb.toString())){
             isEqual = true;
             return;
         }
-        // 백트래킹 : 글자수 넘어 가면 종료
+        
         if(sb.length() == 5) return;
-
-        for(int i = 0; i < sArray.length; i++){
-            sb.append(sArray[i]);
-            result += 1;
-            recursive(word, sb, sArray);
-            //isEqual이 true인 경우, 이미 타겟 문자열을 찾았으므로 재귀 호출을 종료합니다.
+        
+         for(int i = 0; i < alp.length; i++){
+            sb.append(alp[i]);
+            answer += 1;
+            dfs(sb);
             if(isEqual) return;
-            //sb에서 마지막 문자를 제거하여 이전 상태
-            // StringBuilder delete() 메서드는 특정 범위의 문자를 제거합니다.
             sb.delete(sb.length()-1, sb.length());
         }
-
     }
 }
