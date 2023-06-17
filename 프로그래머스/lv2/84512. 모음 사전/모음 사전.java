a79ab2953a80e999a9119e69eea01ffc661a82b2
성호  권 <maystar0503@naver.com>
2023-06-18 08:49:40 +0900

    [level 2] Title: 모음 사전, Time: 42.14 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,31 +1,36 @@
 import java.util.*;
 
 class Solution {
-    static String[] alp = {"A", "E", "I", "O", "U"};
-    static String target;
-    static int answer = 0;
-    static boolean isEqual = false;
+    
+    final char[] WORDS = {'A', 'E', 'I', 'O', 'U'};
+    final int MAX_LENGTH = 5;
+        
     public int solution(String word) {
-        target = word;
-        dfs(new StringBuilder());
+        int answer = 0;
+        
+        List<String> elements = new ArrayList<>();
+        
+        for(int i=0; i<WORDS.length; i++){
+            dfs(elements, String.valueOf(WORDS[i]));
+        }    
+        
+        for(int i=0; i<elements.size(); i++){
+            if(elements.get(i).equals(word)){
+                answer = i + 1;
+                break;
+            }
+        }
+        
         return answer;
     }
     
-    public void dfs(StringBuilder sb){
+    void dfs(List<String> elements, String str){
+        if(str.length() > MAX_LENGTH) return;
         
-        if(target.equals(sb.toString())){
-            isEqual = true;
-            return;
-        }
-        
-        if(sb.length() == 5) return;
-        
-         for(int i = 0; i < alp.length; i++){
-            sb.append(alp[i]);
-            answer += 1;
-            dfs(sb);
-            if(isEqual) return;
-            sb.delete(sb.length()-1, sb.length());
+        if(!elements.contains(str)) elements.add(str);
+    
+        for(int i=0; i<WORDS.length; i++){
+            dfs(elements, str+WORDS[i]);
         }
     }
 }
