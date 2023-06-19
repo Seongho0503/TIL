948577a32ff35be28496ebde19da4ca7abfc67c1
성호  권 <maystar0503@naver.com>
2023-06-19 23:40:08 +0900

    [Bronze II] Title: 커트라인, Time: 132 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -1,5 +1,3 @@
-
-
 import java.util.*;
 import java.io.*;
 
@@ -11,27 +9,14 @@ public class Main {
 		
 		int N = Integer.parseInt(st.nextToken());
 		int cut_num = Integer.parseInt(st.nextToken());
-		int cut_line = 0;
-		int count = 0;
-		
 		
 		st = new StringTokenizer(br.readLine());
 		int[] scores = new int[N];
 		for(int i = 0; i < N; i++) {
 			scores[i] = Integer.parseInt(st.nextToken());
-
 		}
-		
 		Arrays.sort(scores);
-		
-		for(int i = scores.length-1; i >= 0; i--) {
-			count++;
-			if(cut_num == count) {
-				cut_line = scores[i];
-			}
-		}
-		
-		System.out.print(cut_line);
+		System.out.print(scores[N - cut_num]);
 		
 		
 	}
