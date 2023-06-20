4d91f171516db7fab86705fbb626882de12fc5b1
성호  권 <maystar0503@naver.com>
2023-06-21 08:31:06 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 348 ms, Memory: 28044 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -11,12 +11,18 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int[][] arr = new int[N][M];
+		int[][] arr = new int[N+1][M+1];
+		int[][] dp = new int[N+1][M+1];
 		
-		for(int i = 0; i < N; i++) {
+		// DP 배열 할 때 인덱스 범위를 고려하여 +1씩 (인덱스 1부터 시작) : 0 비워둠 
+		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < M; j++) {
+			for(int j = 1; j <= M; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
+				
+				// 각 행(i) 별로 합을 구한다.
+				// dp : 현재 값 + 이전 값들의 합
+				dp[i][j] = arr[i][j] + dp[i][j-1];
 			}
 		}
 		
@@ -29,12 +35,11 @@ public class Main {
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
 			
-			//int sum = (x2 * y2) - (x2-x1)*y2 + (y2-y1)*x2 - x1*y1;
 			int sum = 0;
+			
+			// (중요) 행 i만 for문 돌고 j는 안 돌아도 된다 (dp에서 차를 구하면 된다) 
 			for(int i = x1; i <= x2; i++) {
-				for(int j = y1; j <= y2; j++) {
-					sum += arr[i-1][j-1];
-				}
+				sum += dp[i][y2] - dp[i][y1-1];
 			}
 			sb.append(sum).append('\n');
 		}
