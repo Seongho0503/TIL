5d1b34527541826dc029126dec98a37632222bed
성호  권 <maystar0503@naver.com>
2023-06-21 09:01:19 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 552 ms, Memory: 52992 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,33 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int N = Integer.parseInt(st.nextToken());
-		int count = Integer.parseInt(st.nextToken());
-		int[] nums = new int[N+1];
-		int[] sums = new int[N+1];
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N+1];
+		int[] dp = new int[N+1];
+		
 		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) { // 1부터 처리하면 0인덱스의 i-1을 따로 처리 안해도 됨
-			nums[i] = Integer.parseInt(st.nextToken());
-			// 구간합 공식 = 이전까지의 누적합 + 현재 값
-			sums[i] = sums[i-1] + nums[i];
+		for(int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			// 현재 값 + 이전 값들의 합
+			dp[i] = arr[i] + dp[i-1];
 		}
 		
-		// 해당 범위의 구간합 구하기
-		int sum;
-		for (int i = 1; i <= count; i++) {
+		StringBuilder sb = new StringBuilder();
+		for(int t = 0; t < M; t++) {
 			st = new StringTokenizer(br.readLine());
-			int idx1 = Integer.parseInt(st.nextToken());
-			int idx2 = Integer.parseInt(st.nextToken());
-			// 해당 범위의 구간합 = 해당 범위 마지막 인덱스 구간합 - 시작부붙 전의 인덱스 
-			sum = sums[idx2] - sums[idx1-1];
-			System.out.println(sum);
-		}	
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			sb.append(dp[y] - dp[x-1]).append('\n');
+		}
+		
+		System.out.print(sb);
 	}
 }
