3cba52e55e3a62a883e3bcb2db55f23d27f01c37
성호  권 <maystar0503@naver.com>
2023-06-26 23:21:15 +0900

    [Silver III] Title: 예산, Time: 176 ms, Memory: 15276 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,62 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static long max = Integer.MIN_VALUE;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		long sum = 0;
+
+		int N = Integer.parseInt(br.readLine());
+		
+		long[] arr = new long[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			sum += arr[i];
+			max = Math.max(max, arr[i]);
+		}
+		
+		long money = Integer.parseInt(br.readLine());
+		
+		if(sum <= money) {
+			System.out.println(max);
+		}else {
+			System.out.println(search(arr, money) -1);
+		}
+		
+	}
+	
+	// upBound : 최대값 찾기
+	public static long search(long[] arr, long key) {
+		
+		long start = 0;
+		long end = max;
+		
+		while(start < end) {
+			
+			long mid = (start + end) /2;
+			long sub_sum = 0;
+			
+			for(long val : arr) {
+				if(val < mid) {
+					sub_sum += val;
+				}else {
+					sub_sum += mid;
+				}
+			}
+			if(key < sub_sum) {
+				end = mid;	
+			}else {
+				start = mid + 1;
+			}
+			
+		}
+		return start;
+	}
+
+}
