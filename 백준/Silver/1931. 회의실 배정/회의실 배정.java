a48dfba9fe95068640a86c547dd1f28ffff483c0
성호  권 <maystar0503@naver.com>
2023-06-28 21:11:55 +0900

    [Silver I] Title: 회의실 배정, Time: 536 ms, Memory: 43696 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int [][] arr = new int[N][2];
+		StringTokenizer st;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, (e1,e2) -> {
+			
+			if(e1[1] == e2[1]) {
+				return e1[0] - e2[0];
+			}
+			
+			return e1[1] - e2[1];
+		});
+		
+		int count = 1;
+		int endTime = arr[0][1]; 
+		for(int i = 1; i < N; i++) {
+			if(arr[i][0] >= endTime) {
+				count++; 		
+				endTime = arr[i][1];
+			}
+		}
+		System.out.print(count);	
+	}
+}
