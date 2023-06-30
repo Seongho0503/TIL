fc15e4656d605a8abb49f2cf59b767dd01ca6089
성호  권 <maystar0503@naver.com>
2023-06-30 09:13:22 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 644 ms, Memory: 39296 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -1,16 +1,12 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    
     public static void main(String[] args) throws Exception {
-        new Main().solution();
-    }
-
-    private void solution() throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         List<Word> list = new ArrayList<>();
+        // 단어 , 객체
         Map<String, Word> map = new HashMap<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()); // 개수
@@ -19,15 +15,20 @@ public class Main {
         //while (n-->0) {
         for(int i = 0; i < n; i++) {
         	String cur = br.readLine();
+        	// 거르기
             if (cur.length() < m) continue;
-
+            
+            // 객체 프로퍼티(cnt) 증가
             if (map.containsKey(cur)) {
                 map.get(cur).count();
                 continue;
             }
-
+            
+            // 키(단어)가 없으면 새로운 객체를 생성
             Word word = new Word(cur);
             list.add(word);
+            // 객체를 참조
+            // word의 프로퍼티가 바뀌면 list 안 객체의 프로퍼티도 바뀐다.
             map.put(cur, word);
         }
 
@@ -38,9 +39,9 @@ public class Main {
         }
         System.out.print(sb);
     }
-}
 
-class Word implements Comparable<Word> {
+
+public static class Word implements Comparable<Word> {
     String name;
     int cnt;
 
@@ -65,9 +66,11 @@ class Word implements Comparable<Word> {
 
         return this.name.compareTo(o.name);
     }
-}
+  }
 
- 
+} 
+
 /*
 등장 횟수에 따른 정렬
+
 */
