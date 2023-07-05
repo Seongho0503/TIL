1c5bd1e7f841e2cfc2556e790ac78ce0dca88f65
성호  권 <maystar0503@naver.com>
2023-07-05 23:35:26 +0900

    [Silver II] Title: 이동하기, Time: 544 ms, Memory: 76032 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		
+		
+		int[][] map = new int[N+1][M+1];
+		
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1; j <= M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[][] dp = new int[N+1][M+1]; 
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= M; j++) {
+				dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]) + map[i][j];
+			}
+		}
+		System.out.print(dp[N][M]);
+	}
+}
