1cf3b181bb2640d9aa0e0951890fa2209c3f4cd1
성호  권 <maystar0503@naver.com>
2023-09-11 12:20:06 +0900

    [Silver III] Title: 두 수의 합, Time: 336 ms, Memory: 26216 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -3,34 +3,42 @@ import java.io.*;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
-	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	  int N = Integer.parseInt(br.readLine());
-	  int [] arr = new int[N];
-	  StringTokenizer st = new StringTokenizer(br.readLine());
-	  
-	  // 배열 초기화
-	  for (int i = 0; i < N; i++) {
-		arr[i] = Integer.parseInt(st.nextToken());
-	  }
-	  
-	  int target = Integer.parseInt(br.readLine());
-	  // 정렬 : 투 포인터 규칙 세우기 위해서
-	  Arrays.sort(arr);
-	  int i =0, j =N-1;    // i : start , j : end
-	  int count=0;
-	  // i++ : 앞의 인덱스 뒤로(값 증가) , j-- : 뒤의 인덱스 앞으로 (값 감소)
-	  while(i < j) {
-		  if(arr[i] + arr[j] == target) { // i, j 둘 다 변화 시켜야 함
-			  count++;
-			  i++;
-			  j--;
-		  }else if( arr[i] + arr[j] > target) { // 값 줄여야 함
-			  j--;
-		  }else {  //arr[i] + arr[j] < target 값 증가해야 함
- 			  i++;
-		  }
-	  }
-	  System.out.println(count);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int [n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int target = Integer.parseInt(br.readLine());
+		
+		
+		int start = 0;
+		int end = n-1;
+		int ans = 0;
+		
+		while(end > start) {
+			int sum = arr[start] + arr[end];
+			if(sum == target) {
+				ans++;
+				start++;
+				end--;
+			}
+			else if(sum > target) {
+				end--;
+			}else {	
+				start++;
+			}		
+		}
+		System.out.print(ans);	
 	}
 }
