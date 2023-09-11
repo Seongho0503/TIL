8fc6c47206837a0662616e664db42d2206e8db65
성호  권 <maystar0503@naver.com>
2023-09-11 23:13:02 +0900

    [Silver III] Title: 고양이 카페, Time: 160 ms, Memory: 15620 KB -BaekjoonHub

---
+++ 백준/Silver/28353. 고양이 카페/고양이 카페.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		long target = Integer.parseInt(st.nextToken());
+		
+		st =  new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int start = 0;
+		int end = N-1;
+		long sum = 0;
+		long ans = 0;
+		
+		while(end > start ) {
+			
+			sum = arr[start] + arr[end];
+			
+			if(sum <= target) {
+				start++;
+				end--;
+				ans++;
+			}else {
+				end--;
+			}
+		}
+		
+		System.out.print(ans);
+	}
+}
