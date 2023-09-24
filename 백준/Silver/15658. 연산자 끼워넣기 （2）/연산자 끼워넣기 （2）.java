b046932a4ea8cba7cd9c6215bc2547d0636bc784
성호  권 <maystar0503@naver.com>
2023-09-24 19:07:37 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 144 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.java
@@ -0,0 +1,58 @@
+//package BOJ_14888_실1_연산자끼워넣기;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static int min = Integer.MAX_VALUE;
+	static int max = Integer.MIN_VALUE;
+	static int[] arr;
+	static int[] cal;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		cal = new int[4];
+		for(int i = 0; i < 4; i++) {
+			cal[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dfs(1, arr[0]);
+		System.out.println(max);
+		System.out.println(min);
+	}
+	
+	public static void dfs(int depth, int sum) {
+		
+		if(depth == N) {
+			max = Math.max(sum, max);
+			min = Math.min(sum, min);
+			return;
+		}
+		
+		// {덧셈(+)의 개수, 뺄셈(-)의 개수, 곱셈(×)의 개수, 나눗셈(÷)의 개수}
+		for(int i = 0; i < 4; i++) {
+			
+			// 연산자 개수가 1개 이상인 경우
+			if(cal[i] > 0) {
+				
+				cal[i]--;
+				switch(i) {
+				    case 0: dfs(depth+1, sum + arr[depth]); break;
+				    case 1: dfs(depth+1, sum - arr[depth]); break;
+				    case 2: dfs(depth+1, sum * arr[depth]); break;
+				    case 3: dfs(depth+1, sum / arr[depth]); break;
+				}
+				// 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
+				cal[i]++;	
+			}
+		}
+	}
+}
