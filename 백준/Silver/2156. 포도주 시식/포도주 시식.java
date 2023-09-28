4fcb9e7f4e10ab00a742303a1ec8d2aaaadd9066
성호  권 <maystar0503@naver.com>
2023-09-28 13:03:43 +0900

    [Silver I] Title: 포도주 시식, Time: 152 ms, Memory: 15172 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,37 @@
+//package BOJ_2156_실1_포도주시식;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	static int n;
+	static int[] arr;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n+1];
+		
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int[] dp = new int[n+1];
+		dp[1] = arr[1];
+		
+		// 포도주가 1잔인 경우에 indexArrayOUT 에러남
+		if(n >= 2) {
+			dp[2] = dp[1] + arr[2];
+			//dp[2] = Math.max(dp[1] + arr[2], dp[1]);
+		}
+	
+		for(int i = 3; i <= n; i++) {
+			dp[i] = Math.max(dp[i-1], Math.max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i]);
+			//dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]));
+		}
+
+		System.out.println(dp[n]);
+	}
+}
