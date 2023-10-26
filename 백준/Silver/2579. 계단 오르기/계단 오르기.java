5e8b5afc77b22311cd8ade60c247a1a362586864
성호  권 <maystar0503@naver.com>
2023-10-26 13:18:10 +0900

    [Silver III] Title: 계단 오르기, Time: 116 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,38 +1,35 @@
-//package BOJ_2527_실3_계단오르기;
+//package BOJ_2579_실3_계단오르기;
 
 import java.io.*;
 import java.util.*;
 
 public class Main {
 	
-	static int n;
-	static int[] arr;
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n+1];
 		
-		for(int i = 1; i <= n; i++) {
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N+1];
+		
+		for(int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
-		int[] dp = new int[n+1];
+		int[] dp = new int[N+1];
+		
 		dp[1] = arr[1];
-		
-		if(n >= 2) {
-			dp[2] = dp[1] + arr[2];
-			//dp[2] = Math.max(dp[1] + arr[2], dp[1]);
+		// dp[2] = arr[2] + dp[1]; Arrays.outOfRange
+		if(N >= 2) {
+			dp[2] = arr[2] + dp[1];
 		}
 		
-		for(int i = 3; i <= n; i++) {
-			dp[i] = Math.max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i];
-			//dp[i] = Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]);
+		for(int i = 3; i <= N; i++) {
+			dp[i] = Math.max(dp[i-3] + arr[i-1] , dp[i-2]) + arr[i];
 		}
 		
-//		for(int c : dp) {
-//			System.out.print(c + " ");
-//		}
-		System.out.println(dp[n]);
+		//System.out.println(Arrays.toString(dp)); 디버깅
+		System.out.println(dp[N]);
 	}
+
 }
