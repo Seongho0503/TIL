9984d3787a65bc0675f3697c9c72b323968d2623
성호  권 <maystar0503@naver.com>
2023-11-09 07:33:27 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,46 +1,55 @@
 class Solution {
-    static int[] discount = {10,20,30,40};
-    static int maxOfSubscribe;
-    static int maxOfCost;
+    static int[] discount = {10, 20, 30, 40};
     static int len;
+    static int maxCost;
+    static int maxSubscribe;
+    static int[] discounts;
     public int[] solution(int[][] users, int[] emoticons) {
-        len = emoticons.length;
-        findResult(0, new int[emoticons.length], users, emoticons);
-        return new int[]{maxOfSubscribe,maxOfCost};
-    }
 
-    public void findResult(int depth, int[] discounts,int[][] users, int[] emoticons){
-        if (depth == len){
+        len = emoticons.length;
+        discounts = new int[len];
+        
+        dfs(0, users, emoticons);
+        return new int[] {maxSubscribe, maxCost};
+    }
+    
+    static void dfs(int depth, int[][] users, int[] emoticons){
+        
+        if(depth == len){
             int subscribe = 0;
             int cost = 0;
-
-            for (int[] user: users){
+            
+            for(int[] user : users){
                 int userDiscountRate = user[0];
                 int userMaxCost = user[1];
-
+                
                 int sum = 0;
-
-                for (int i = 0; i < emoticons.length; i++){
-                    if (discounts[i]>=userDiscountRate){
-                        sum += emoticons[i]/100*(100-discounts[i]);
+                
+                for(int i = 0; i < emoticons.length; i++){
+                    if(discounts[i] >= userDiscountRate){
+                        sum += emoticons[i] / 100 * (100 - discounts[i]); 
                     }
                 }
-                if (sum>=userMaxCost)subscribe++;
-                else cost+=sum;
-            }
-            if (subscribe>maxOfSubscribe){
-                maxOfSubscribe = subscribe;
-                maxOfCost = cost;
-            }else if (subscribe == maxOfSubscribe){
-                maxOfCost = Math.max(maxOfCost,cost);
-            }
+                
+                if(sum >= userMaxCost ) subscribe++;
+                else cost += sum;
+                       
+            } // user
+            
+            if(subscribe > maxSubscribe ) {
+                maxSubscribe = subscribe;
+                maxCost = cost;
+            } else if (subscribe == maxSubscribe && cost > maxCost){
+                maxCost = cost;
+            } 
+            
             return;
         }
         
-        
-        for (int i = 0; i < 4; i++){
-            discounts[depth] = discount[i];
-            findResult(depth + 1, discounts, users, emoticons);
+        for(int i = 0; i < 4; i++){
+            discounts[depth] = discount[i]; 
+            dfs(depth+1, users, emoticons);
         }
+        
     }
 }
